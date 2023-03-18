e870b54650f0c414dd282c2393d80ed4aed10d19
Margit Antal <margitantal68@gmail.com>
2023-03-18 12:50:22 +0200

    registration

--- registration/main_registration.cpp
+++ registration/main_registration.cpp
@@ -7,5 +7,32 @@
 using namespace std;
 
 int main() {
+    map<string, int> names;
+    int n;
+    string line;
+    getline(cin, line);
+    istringstream iss(line);
+    iss >> n;
+    for (int i = 0; i < n; ++i) {
+        string name;
+        getline(cin, name);
+        if (names.find(name) == names.end()) { // nem talalhato a name a names taroloban
+            cout << "OK" << endl;
+            names[name] = 1;
+//            names.insert({name, 1});
+        } else{
+            int counter = names[ name ];
+
+            stringstream oss(name);
+            oss << name << counter;
+            string newname;
+            oss >> newname;
+            cout << newname << endl;
+
+            names[name]++; // novelem a kulcs elofordulasi szamat
+
+        }
+    }
+
     return 0;
 }
